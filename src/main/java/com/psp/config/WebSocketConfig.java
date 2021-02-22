d97de67aa50254a6578e356784d83cd6538c61c2
Đoàn Quang Hòa <hoa9x3@gmail.com>
2021-02-22 13:26:15 +0700

    Update WebSocketConfig.java

--- src/main/java/com/psp/config/WebSocketConfig.java
+++ src/main/java/com/psp/config/WebSocketConfig.java
@@ -8,9 +8,6 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-/**
- * Created by clevekim@naver.com on 2019-05-13
- */
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
